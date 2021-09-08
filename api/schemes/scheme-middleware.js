5594b088a9a36522a78960f01d4d6eeadca47d32
Bradly Lewis <bradlylewisdev@gmail.com>
2021-09-08 17:40:01 -0400

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -10,11 +10,17 @@ const db = require('../../data/db-config')
 */
 const checkSchemeId = async (req, res, next) => {
   try {
-    const existing = await db('schemes').where('sheme_id', req.params.scheme_id).first()
+    const existing = await db('schemes')
+    .where('scheme_id', req.params.scheme_id)
+    .first()
+
     if(!existing) {
-      res.status(404).json({
-        message: `scheme with ${req.params.scheme_id} not found`
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`
       })
+    } else {
+      next()
     }
   } catch(err) {
     next(err)
@@ -30,7 +36,12 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if(scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
+    next({ status: 400, message: 'invalid scheme_name' })
+  } else {
+    next()
+  }
 }
 
 /*
@@ -43,7 +54,19 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const { instructions, step_number } = req.body
+  if(
+    instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const error = { status: 400, message: 'invalid step' }
+    next(error)
+  } else {
+    next()
+  }
 }
 
 module.exports = {
--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -17,7 +17,12 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
- return db('schemes')
+    return db("schemes as sc")
+      .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id" )
+      .select("sc.*")
+      .count("st.step_id as number_of_steps")
+      .groupBy("sc.scheme_id")
+      .orderBy("sc.scheme_id", "asc")
 }
 
 async function findById(scheme_id) { // EXERCISE B
@@ -86,11 +91,31 @@ async function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
-      const result = await db('schemes').where('id', scheme_id).first()
-      return result
+  const rows = await db("schemes as sc")
+  .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id" )
+  .where('sc.scheme_id', scheme_id)
+  .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+  .orderBy('st.step_number')
+
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: []
+  }
+  
+  rows.forEach(row => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions,
+      })
+    }
+  })
+  return result
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -111,12 +136,25 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+
+  const rows = await db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+    .where('sc.scheme_id', scheme_id)
+    .orderBy('step_number')
+
+  if (!rows[0].step_id) return []
+  return rows
 }
 
 function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+ return db('schemes').insert(scheme)
+  .then(([scheme_id]) => {
+    return db('schemes').where('scheme_id', scheme_id).first()
+  })
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
@@ -125,6 +163,17 @@ function addStep(scheme_id, step) { // EXERCISE E
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+ return db('steps').insert({
+   ...step,
+   scheme_id
+ })
+  .then(() => {
+    return db('steps as st')
+    .join('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
+    .select('step_id', 'step_number', 'instructions', 'scheme_name')
+    .orderBy('step_number')
+    .where('sc.scheme_id', scheme_id)
+  })
 }
 
 module.exports = {
